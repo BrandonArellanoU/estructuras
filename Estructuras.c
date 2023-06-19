72dd4d7f20e1e345c4effef834d08a58e6f3b3e4
Brandon Arellano <brandon.arellano@udla.edu.ec>
2023-06-19 16:19:53 -0500

    estructura para imprimir los datos

--- Estructuras.c
+++ Estructuras.c
@@ -13,10 +13,6 @@ struct colecionEstructura
 
 int main(){
      struct colecionEstructura estructura_Alumno[2];
-    
-    
-
-    
 
     for (int i = 0; i < 2; i++)
     {
@@ -24,30 +20,29 @@ int main(){
         printf("Ingrese los datos del Alumno:\n");
         printf("Nombre:");
         fflush(stdin);
-        gets(estructura_Alumno.nombre);
+        gets(estructura_Alumno->nombre);
         printf("Direccion:");
         fflush(stdin);
-        gets(estructura_Alumno.direccion);
+        gets(estructura_Alumno->direccion);
         printf("Carrera:");
         fflush(stdin);
-        gets(estructura_Alumno.carrera);
+        gets(estructura_Alumno->carrera);
         printf("Edad:");
         fflush(stdin);
-        scanf("%i",&estructura_Alumno.edad[i]);
+        scanf("%i",&estructura_Alumno->edad);
         printf("Promedio:");
         fflush(stdin);
-        scanf("%0.2f",&estructura_Alumno.promedio[i]);
+        scanf("%0.2f",&estructura_Alumno->promedio);
     }     
 
     for (int i = 0; i < 2; i++)
     {
-        printf("Datos del Alumno:\n",estructura_Alumno);
-
-        puts(estructura_Alumno.nombre[2]);
-        puts(estructura_Alumno.direccion[2]);
-        puts(estructura_Alumno.carrera[2]);
-        printf("%i\n", estructura_Alumno.edad[2]);
-        printf("%0.2f\n", estructura_Alumno.promedio[2]);
+        printf("Datos del Alumno:\n",estructura_Alumno[i]);
+        puts(estructura_Alumno->nombre);
+        puts(estructura_Alumno->direccion);
+        puts(estructura_Alumno->carrera);
+        puts(estructura_Alumno->edad);
+        puts(estructura_Alumno->promedio);
     }
     
 
